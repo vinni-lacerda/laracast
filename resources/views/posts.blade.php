6fe13e09ae92d90d1371040723a7391513a758f7
viniciuslacerda <vini.lacerda@hotmail.com>
2026-05-21 15:57:27 -0300

    refactor: turning posts page dynamic with each post

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -3,16 +3,9 @@
 {{--@vite('resources/css/app.css')--}}
 <link rel="stylesheet" href="/app.css">
 <body>
-    <article>
-        <a href="/posts/my-first-post"><h1>First post</h1></a>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec imperdiet elit odio, ac interdum felis gravida a. Aenean magna velit, volutpat eget mauris sit amet, porta tempus ipsum. Curabitur dapibus, tortor egestas luctus volutpat, mi ante pretium elit, nec aliquet orci metus porta justo. Ut accumsan in sem ut aliquam. Cras ut gravida lectus, eleifend ullamcorper tortor. Duis ullamcorper felis vel orci sodales rutrum. Donec urna lorem, placerat ac pretium vitae, pellentesque maximus sem. Nulla aliquet, turpis ut vulputate auctor, libero justo molestie leo, elementum suscipit libero massa ac erat. Proin interdum, nulla sed volutpat ullamcorper, sem lacus consectetur nulla, in tempor diam tortor at mi. Aenean lobortis justo eu imperdiet lacinia.</p>
-    </article>
-    <article>
-        <a href="/posts/my-second-post"><h1>Second post</h1></a>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec imperdiet elit odio, ac interdum felis gravida a. Aenean magna velit, volutpat eget mauris sit amet, porta tempus ipsum. Curabitur dapibus, tortor egestas luctus volutpat, mi ante pretium elit, nec aliquet orci metus porta justo. Ut accumsan in sem ut aliquam. Cras ut gravida lectus, eleifend ullamcorper tortor. Duis ullamcorper felis vel orci sodales rutrum. Donec urna lorem, placerat ac pretium vitae, pellentesque maximus sem. Nulla aliquet, turpis ut vulputate auctor, libero justo molestie leo, elementum suscipit libero massa ac erat. Proin interdum, nulla sed volutpat ullamcorper, sem lacus consectetur nulla, in tempor diam tortor at mi. Aenean lobortis justo eu imperdiet lacinia.</p>
-    </article>
-    <article>
-        <a href="/posts/my-third-post"><h1>Third post</h1></a>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec imperdiet elit odio, ac interdum felis gravida a. Aenean magna velit, volutpat eget mauris sit amet, porta tempus ipsum. Curabitur dapibus, tortor egestas luctus volutpat, mi ante pretium elit, nec aliquet orci metus porta justo. Ut accumsan in sem ut aliquam. Cras ut gravida lectus, eleifend ullamcorper tortor. Duis ullamcorper felis vel orci sodales rutrum. Donec urna lorem, placerat ac pretium vitae, pellentesque maximus sem. Nulla aliquet, turpis ut vulputate auctor, libero justo molestie leo, elementum suscipit libero massa ac erat. Proin interdum, nulla sed volutpat ullamcorper, sem lacus consectetur nulla, in tempor diam tortor at mi. Aenean lobortis justo eu imperdiet lacinia.</p>
-    </article>
+    <?php foreach ($posts as $post) : ?>
+        <article>
+            <?= $post; ?>
+        </article>
+    <?php endforeach; ?>
 </body>
