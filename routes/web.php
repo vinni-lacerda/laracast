fbc31b9ece4865f6d0342096cf747ef163659552
viniciuslacerda <vini.lacerda@hotmail.com>
2026-05-21 15:58:22 -0300

    refactor: routes  dynamic returning post model methods

--- routes/web.php
+++ routes/web.php
@@ -1,24 +1,20 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\Post;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('/posts', function() {
-    return view('posts');
-});
-Route::get('posts/{post}', function($slug) {
-    $path = __DIR__ . "/../resources/posts/{$slug}.html";
-
-    if(! file_exists($path)){
-        abort(404);
-    }
-
-    $post = file_get_contents($path);
-
-    return view('post', [
-        'post' => $post
+    return view('posts', [
+        'posts' => Post::all()
     ]);
 });
+Route::get('posts/{post}', function($slug) {
+    return view('post', [
+        'post' => Post::find($slug)
+    ]);
+
+})->where('post', '[A-z_\-]+');
